fb219c8a3df334fa5aa311f1ae5ee51cdf28d038
malloryporhce <malloryburke@gmail.com>
2018-02-24 22:20:55 -0800

    Added mongo database connection

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,23 @@ router.post('/register', function(req, res, next){
           err.status = 400;
           return next(err);
           }
+
+          //  create object with form input
+          var userData = {
+            email: req.body.email,
+            name: req.body.name,
+            favoriteBook: req.body.favoriteBook,
+            password: req.body.password
+          };
+
+          // use schema's 'create' method to insert document into mongo
+          User.create(userData, function (error, user) {
+            if (error) {
+              return next(error);
+            } else {
+              return res.redirect('/profile');
+            }
+          });
         } else {
        var err = new Error('All fields required.');
        err.status = 400;
